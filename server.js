72793b1baa91fb14d205f9e737f2034bc018c73f
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-10-23 12:53:32 -0400

    Adds update customer by id route.

--- server.js
+++ server.js
@@ -22,28 +22,28 @@ MongoClient.connect(url, (err, db) => {
 		console.log('Watching on port ', port); 
 	}); 
 	
-	//connect routes to database
-	// app.post('/customers', urlEncodedParser, (req, res) => {
-	// 	db.collection('customers').insert({
-	// 		firstname: req.body.firstname, 
-	// 		lastname: req.body.lastname, 
-	// 		order: req.body.order
-	// 	});
-	// 	res.send(req.body.firstname + ' ' + req.body.lastname + ' was added to the customer database.');
-	// }); 
+	// connect routes to database
+	app.post('/customers', urlEncodedParser, (req, res) => {
+		db.collection('customers').insert({
+			firstname: req.body.firstname, 
+			lastname: req.body.lastname, 
+			order: req.body.order
+		});
+		res.send(req.body.firstname + ' ' + req.body.lastname + ' was added to the customer database.');
+	}); 
 
-	// app.get('/customers', (req, res) => {
-	// 	db.collection('customers').find({}) //returns cursor
-	// 	.toArray(function (err, customers) { //iterate cursor
-	//          if (err) {
-	//            reject(err);
-	//          } else {
-	//            res.send({customers});
-	//          }          
-	//     }); 
-	// });
+	app.get('/customers', (req, res) => {
+		db.collection('customers').find({}) //returns cursor
+		.toArray(function (err, customers) { //iterate cursor
+	         if (err) {
+	           reject(err);
+	         } else {
+	           res.send({customers});
+	         }          
+	    }); 
+	});
 
-	app.put('/customers/:id', (req, res) => {
+	app.put('/customers/:id', urlEncodedParser, (req, res) => {
 		const id = req.params.id;
 		db.collection('customers').find({ '_id' : ObjectId(id) }, function(err, item) {
 			if (err) { console.log('first err') } 
@@ -52,7 +52,11 @@ MongoClient.connect(url, (err, db) => {
 					throw err;
 				} else {
 					if (array.length > 0) {
-						res.status(200).send(array);
+						res.status(200).send({
+							firstname: req.body.firstname, 
+							lastname: req.body.lastname, 
+							order: req.body.order
+						});
 					} else {
 						res.status(404).send(`Customer ${id} not found.`)
 					}
