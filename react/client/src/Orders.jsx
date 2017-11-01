849fec56e708833c94e93a752c7a0444f699501f
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-11-01 09:36:30 -0400

    Bootstrap columns not working.

--- react/client/src/Orders.jsx
+++ react/client/src/Orders.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class Orders extends Component {
-
   render() {
     const customers = this.props.customers;
     const orderDisplayMessage = this.props.orderDisplayMessage;
@@ -10,14 +9,26 @@ class Orders extends Component {
       <div>
         <h5>Orders</h5>
         <p>{orderDisplayMessage}</p>
-          {customers.map((customer) => {
-            return <div key={customer.firstname + customer.lastname}>
-              {customer.firstname} {customer.lastname} {customer.order}
+          <div class="container">
+            <div class="row">
+              <div class="col-sm">
+                1 of 3 columns
+              </div>
+              <div class="col-sm">
+                2 of 3 columns
+              </div>
+              <div class="col-sm">
+                3 of 3 columns
+              </div>
             </div>
-          })} 
+          </div>
       </div>
     );
   }
 }
 
 export default Orders;
+
+// {customers.map((customer) => {
+//             return key={customer.firstname + customer.lastname}
+//             {customer.firstname} {customer.lastname} {customer.order}       })} 
